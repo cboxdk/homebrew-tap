6040bf83fe7711b6d4256f626dce5e5c980213a1
Cbox <sn@cbox.dk>
2026-08-30 20:02:39 +0000

    fpm-tune 0.1.0-beta.6

--- Formula/fpm-tune.rb
+++ Formula/fpm-tune.rb
@@ -11,28 +11,28 @@
 class FpmTune < Formula
   desc "Autotune PHP-FPM pools against a host's real memory; reload, never restart"
   homepage "https://github.com/cboxdk/fpm-tune"
-  version "0.1.0-beta.5"
+  version "0.1.0-beta.6"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.5/fpm-tune-0.1.0-beta.5-darwin-arm64.tar.gz"
-      sha256 "4b1a677d88ce6c30148cebfc66761eed1e6ce8275f007253bbb70d3ac06edcfe"
+      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.6/fpm-tune-0.1.0-beta.6-darwin-arm64.tar.gz"
+      sha256 "693249e043ca0bb114bf778525814a4ce68634cf60523623c2f78a996004bfe4"
     end
     on_intel do
-      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.5/fpm-tune-0.1.0-beta.5-darwin-amd64.tar.gz"
-      sha256 "34ffd04baca9d9d87c83b2a77dc3bb1693d1c7714a0e4ec5f51ced40781fcfbc"
+      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.6/fpm-tune-0.1.0-beta.6-darwin-amd64.tar.gz"
+      sha256 "d131b640de9d40ee1f9b928ce536d7b4bfad80c4f539d86c5ecbfb01375b48a1"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.5/fpm-tune-0.1.0-beta.5-linux-arm64.tar.gz"
-      sha256 "79f3bff5f45529f67174a8273dccfd9b581319256b8d16a26dac7d5f7509930a"
+      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.6/fpm-tune-0.1.0-beta.6-linux-arm64.tar.gz"
+      sha256 "3296ef257b597e6365d69a51d3ef48d68c1ec927830a533fb28eb19d46b9c9e3"
     end
     on_intel do
-      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.5/fpm-tune-0.1.0-beta.5-linux-amd64.tar.gz"
-      sha256 "0997d839985593b4695402a8114c7d19787fcd9bbcbec828eb0ea0a89bcd5f37"
+      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.6/fpm-tune-0.1.0-beta.6-linux-amd64.tar.gz"
+      sha256 "faffcef73263c6c86d59d38c7e5652e210a8a0bba992cfc43562d69e99a68425"
     end
   end
 
