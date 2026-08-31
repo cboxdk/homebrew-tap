c5a3d7f40c85dc560432a18c488425a20fb8662b
Cbox <sn@cbox.dk>
2026-08-31 17:50:55 +0000

    fpm-tune 0.1.0-beta.10

--- Formula/fpm-tune.rb
+++ Formula/fpm-tune.rb
@@ -11,28 +11,28 @@
 class FpmTune < Formula
   desc "Autotune PHP-FPM pools against a host's real memory; reload, never restart"
   homepage "https://github.com/cboxdk/fpm-tune"
-  version "0.1.0-beta.9"
+  version "0.1.0-beta.10"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.9/fpm-tune-0.1.0-beta.9-darwin-arm64.tar.gz"
-      sha256 "a66b18438994f7134008a6bffe8034b113098e93b0df0a9c4b534fd42539b1c8"
+      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.10/fpm-tune-0.1.0-beta.10-darwin-arm64.tar.gz"
+      sha256 "7f0c14f4d680aba961ad7129c0d5aa3d043907f6310db75a81bd6d8de8ca1b53"
     end
     on_intel do
-      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.9/fpm-tune-0.1.0-beta.9-darwin-amd64.tar.gz"
-      sha256 "b2b53c59f8c40f71a883a21af5069dc823a151ef1c774dea77236ca7fd01729d"
+      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.10/fpm-tune-0.1.0-beta.10-darwin-amd64.tar.gz"
+      sha256 "38cb059aba75d57831757a09058739828ca57484c06559bf410c48183d26675b"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.9/fpm-tune-0.1.0-beta.9-linux-arm64.tar.gz"
-      sha256 "8ecd757f26ae60227ab6c5a976fc343639724992aaaf1a75ff5b510dcee3d492"
+      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.10/fpm-tune-0.1.0-beta.10-linux-arm64.tar.gz"
+      sha256 "263337711275b9caf31493fcfa9aed1e2b0ff2e9ab46bba199dbb0a6aebda491"
     end
     on_intel do
-      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.9/fpm-tune-0.1.0-beta.9-linux-amd64.tar.gz"
-      sha256 "692813ce0a35fd5a250e3815a4c49901752ca13ca5ac43a5090b7edbc9284588"
+      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.10/fpm-tune-0.1.0-beta.10-linux-amd64.tar.gz"
+      sha256 "9cace7e5d6a41553029a5148f7ac3c368388f7090ba260e0945e39567dc3a229"
     end
   end
 
