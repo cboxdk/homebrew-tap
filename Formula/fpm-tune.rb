703c0ae0f7735673a5d2b0c5d0a127eb2e4660d7
Cbox <sn@cbox.dk>
2026-08-31 19:41:45 +0000

    fpm-tune 0.1.0-beta.14

--- Formula/fpm-tune.rb
+++ Formula/fpm-tune.rb
@@ -11,28 +11,28 @@
 class FpmTune < Formula
   desc "Autotune PHP-FPM pools against a host's real memory; reload, never restart"
   homepage "https://github.com/cboxdk/fpm-tune"
-  version "0.1.0-beta.13"
+  version "0.1.0-beta.14"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.13/fpm-tune-0.1.0-beta.13-darwin-arm64.tar.gz"
-      sha256 "1f96c1b4a19b22d8f8abcf03101253ff25620a46b0db4658769d033f355059a2"
+      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.14/fpm-tune-0.1.0-beta.14-darwin-arm64.tar.gz"
+      sha256 "3f5b6018acdb761e369449db10f123953a0b64effa45e07d0e8d9b5e6699b500"
     end
     on_intel do
-      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.13/fpm-tune-0.1.0-beta.13-darwin-amd64.tar.gz"
-      sha256 "1580b31c61984e685532b3d1d4cff1fa614682331342387568da9b8d3356e5e0"
+      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.14/fpm-tune-0.1.0-beta.14-darwin-amd64.tar.gz"
+      sha256 "a46cc4e8aa69f63f03b31f003f47f91d642ef5398491549192b6a0d45cf54a17"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.13/fpm-tune-0.1.0-beta.13-linux-arm64.tar.gz"
-      sha256 "035903834c78d95301f10a74625f7bf54cf0abfd238cae68422e6c1786a152b6"
+      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.14/fpm-tune-0.1.0-beta.14-linux-arm64.tar.gz"
+      sha256 "3defffabe04da438452acdc1b39be49b276644b7d3da65c706261556eddfbeb2"
     end
     on_intel do
-      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.13/fpm-tune-0.1.0-beta.13-linux-amd64.tar.gz"
-      sha256 "6bd94d83f219544b79c7d6c83cd05d7e88c88f1bced5a114842e01825b2eeb71"
+      url "https://github.com/cboxdk/fpm-tune/releases/download/v0.1.0-beta.14/fpm-tune-0.1.0-beta.14-linux-amd64.tar.gz"
+      sha256 "b71811af813fb89d2935b0790907c252a5cb698f4a6913b77e59934c41287ba0"
     end
   end
 
